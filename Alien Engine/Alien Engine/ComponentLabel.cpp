0d12377e56a893a567478adaabd82f307bdf6447
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2019-12-24 00:04:48 +0100

    putting functions to ComponentLabel

--- Alien Engine/Alien Engine/ComponentLabel.cpp
+++ Alien Engine/Alien Engine/ComponentLabel.cpp
@@ -1,10 +1,46 @@
 #include "ComponentLabel.h"
+#include "ComponentTransform.h"
+#include "GameObject.h"
+#include "Application.h"
 
-ComponentLabel::ComponentLabel(GameObject* attached) :Component(attached)
+ComponentLabel::ComponentLabel(GameObject* attach, float2 size) :Component(attach)
 {
 	type = ComponentType::LABEL;
+	this->size = size;
+	this->size_text = size;
+	if (size.x == 0)
+	{
+		this->size.x = 20;
+		this->size.y = 10;
+	}
 }
 
 ComponentLabel::~ComponentLabel()
 {
+}
+
+void ComponentLabel::Update()
+{
+}
+
+void ComponentLabel::PostUpdate()
+{
+	Draw();
+}
+
+void ComponentLabel::Draw()
+{
+}
+
+void ComponentLabel::SaveComponent(JSONArraypack* to_save)
+{
+}
+
+void ComponentLabel::LoadComponent(JSONArraypack* to_load)
+{
+}
+
+bool ComponentLabel::DrawInspector()
+{
+	return false;
 }
--- Alien Engine/Alien Engine/ComponentLabel.h
+++ Alien Engine/Alien Engine/ComponentLabel.h
@@ -5,6 +5,18 @@
 class ComponentLabel :public Component
 {
 public:
-	ComponentLabel(GameObject* attached);
+	ComponentLabel(GameObject* attach, float2 size);
 	~ComponentLabel();
+
+	void Update();
+	void PostUpdate();
+	void Draw();
+
+	void SaveComponent(JSONArraypack* to_save);
+	void LoadComponent(JSONArraypack* to_load);
+
+	bool DrawInspector();
+
+private:
+	float2 size_text;
 };
